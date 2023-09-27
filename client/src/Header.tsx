1b641cd3414b23762dcc64407fdfe1e61a650c69
varun146 <varungarg63683@gmail.com>
2023-09-27 10:44:01 +0530

    adding a header

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <header className='header'>Hello </header>
-  )
-}
+    <header className="header">
+      <div className="container">
+        <div>
+          <a href="/">LOGO</a>
+        </div>
+        <div>
+          <a href="/">Decks</a>
+        </div>
+        <div>
+          <a href="/">Login</a>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
